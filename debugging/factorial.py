7d209e36337390f0803e7e4b22aa0b2b8bec88c4
mohamedklby <moha.klby333@gmail.com>
2025-01-17 00:35:41 +0100

    kjvvkj

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -4,11 +4,9 @@ import sys
 def factorial(n):
     result = 1
     while n > 1:
-        result *= n  # Multiplie 'result' par 'n' à chaque itération
-        n -= 1  # Décrémente 'n' pour réduire la valeur jusqu'à 1
+        result *= n
+        n -= 1  # Decrement n to avoid an infinite loop
     return result
 
-# Récupère l'argument de la ligne de commande, convertit en entier, et calcule sa factorielle
 f = factorial(int(sys.argv[1]))
-print(f)  # Affiche le résultat de la factorielle
-
+print(f)
